9553d8987286ad8b10e64ccf9ba907f1e88da25e
human-gd <rezerve643@gmail.com>
2026-08-17 15:57:31 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+$on_mod(Loaded) {
+    log::info("================================");
+    log::info("Adled Manix loaded!");
+    log::info("Version: 1.0.0");
+    log::info("Geometry Dash: 2.2081");
+    log::info("Geode: 5.9.0");
+    log::info("================================");
+}
